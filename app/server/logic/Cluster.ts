24fb2ea54a206c5762d8a1cbc754df618e888a10
tamir303 <tamir.spilberg@s.afeka.ac.il>
2023-08-03 00:26:55 +0300

    logic in progress

--- app/server/logic/Cluster.ts
+++ app/server/logic/Cluster.ts
@@ -3,10 +3,36 @@ import { ClusterObjectType } from "@/app/types"
 export class Cluster {
     private _fields: string[]
     private _values: string[][]
+    private _centroid: number[] = []
 
     constructor({ fields, values }: ClusterObjectType) {
         this._fields = fields
         this._values = values
+        this._centroid = this.findCentroid()
+    }
+
+    get fields() {
+        return this.fields
+    }
+
+    get values() {
+        return this._values
+    }
+
+    get centroid() {
+        return this._centroid
+    }
+
+    set fields(fields: string[]) {
+        this._fields = fields
+    }
+
+    set values(values: string[][]) {
+        this._values = values;
+    }
+
+    set centroid(centroid) {
+        this._centroid = centroid;
     }
 
     /**
@@ -17,31 +43,45 @@ export class Cluster {
         N is the total number of data points.
         x_i,d is the value of the data point i along dimension d.
     */
-    centroid() {
+    findCentroid() {
+        // For each field (dᵢ) calculate the average
+        const N = this._values.length;
+        const D = this._fields.length;
         return Array
-            .from({ length: this._fields.length }, (_, index) => index) // For each field (dᵢ)
+            .from({ length: D }, (_, index) => index)
             .map((index) => {
-                return this._values
+                return (1 / N) * this._values
                     .reduce((sum, valueArray) => sum + Number(valueArray[index]), 0)
             })
     };
-}
 
-/**
- * WCSS = Σᵢ Σₓ₌₁ⁿ (x - μᵢ)²
-    Where:
+    /**
+    * Variance = (1 / N) * Σᵢ Σᵈ (xᵢ₋ᵈ - μᵈ)²
+        Where:
 
-    WCSS: Within-Cluster Sum of Squares
-    Σᵢ: Summation over all clusters (i = 1 to K)
-    Σₓ₌₁ⁿ: Summation over all data points within cluster i (x = 1 to n)
-    x: Data point within cluster i
-    μᵢ: Centroid (mean) of cluster i
- */
-variance() {
+        Variance: The variance of the cluster.
+        N: The total number of data points in the cluster.
+        Σᵢ: Summation over all data points in the cluster (i = 1 to N).
+        Σᵈ: Summation over all dimensions (d = 1 to D).
+        xᵢ₋ᵈ: The value of data point i along dimension d.
+        μᵈ: The value of the centroid along dimension d.
+    */
+    findVariance() {
+        const N = this._values.length;
+        const D = this._fields.length;
+        const centroid = this._centroid;
 
-}
+        // Calculate the variance for each dimension
+        const variance = this._values.reduce((acc, valueArray) => {
+            return acc.map((sum, d) => sum + Math.pow(Number(valueArray[d]) - centroid[d], 2));
+        }, Array.from({ length: D }, () => 0));
 
-nextIter() {
+        // Divide by N to get the average variance
+        return variance.map((sum) => (1 / N) * sum);
+    }
 
-}
+    nextIter(values: string[][]) {
+        this._values = values
+        this._centroid = this.findCentroid()
+    }
 }
