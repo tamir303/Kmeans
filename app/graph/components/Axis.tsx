78bc5a5e36b565fdefc2b60feae59a148a552c3a
tamir303 <tamir.spilberg@s.afeka.ac.il>
2023-08-08 15:29:57 +0300

    found way to create series, clusters are undefined after first iteration

--- app/graph/components/Axis.tsx
+++ app/graph/components/Axis.tsx
@@ -3,76 +3,37 @@
 
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import { AgChartsReact } from "ag-charts-react";
-import { DataObjectType } from "@/app/types";
+import { ClusterObjectType, DataObjectType } from "@/app/types";
 
 const Axis: React.FC<DataObjectType> = ({ iter, k, clusters }) => {
   const axisRef = useRef(null);
   const numOfPoints = clusters.flatMap((cluster) => cluster.values).length;
-  const [scatterSeriesConfigs, setScatterSeriesConfigs] = useState<any[]>([]);
-  const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
 
-  console.log(clusters);
+  const generateChartData = (clusters: ClusterObjectType[]) => {
+    const colors = ['red', 'green', 'blue', 'orange', 'purple']; // Add more colors if needed
 
-  useEffect(() => {
-    // Map through clusters and create a scatter series config for each cluster
-    const newScatterSeriesConfigs = clusters.map((cluster, index) => {
-      const color = colors[index % colors.length]; // Pick a color from the array based on index
-
-      const fieldNames = cluster.fields || []; // Get the field names for the current cluster
-
-      // Extract the data (nested arrays) from the current cluster
-      const data = cluster.values.map((input) => ({
-        [fieldNames[0]]: parseFloat(input[0]), // Assuming the first element is X coordinate (you can adjust accordingly)
-        [fieldNames[1]]: parseFloat(input[1]), // Assuming the second element is Y coordinate (you can adjust accordingly)
-        color, // Use the color for this cluster
-      }));
-
+    /** @todo: Issue, values are undefined after running first itermation */
+    const chartData = clusters.map((cluster, index) => {
+      const { fields, values } = cluster;
       return {
-        type: "scatter",
+        data: values.map((valueArray) => {
+          return Object.fromEntries(fields.map((field, index) => [field, valueArray[index]]));
+        }),
+        type: 'scatter',
+        xKey: `${fields[0]}`,
+        yKey: `${fields[1]}`,
         marker: {
-          type: "circle",
-          size: 12,
-        },
-        xKey: fieldNames[0],
-        yKey: fieldNames[1],
-        title: `Cluster ${index + 1} (${fieldNames.join(", ")})`, // Include field names in the series title
-        data,
-      };
+          type: 'circle',
+          fill: colors[index % colors.length]
+        }
+      }
     });
 
-    setScatterSeriesConfigs(newScatterSeriesConfigs);
-  }, [clusters]);
+    return chartData;
+  };
 
   const [options, setOptions] = useState({
-    title: {
-      text: `${numOfPoints} Inputs on Axis`,
-    },
-    subtitle: {
-      text: "",
-    },
-    series: scatterSeriesConfigs,
-    axes: [
-      {
-        type: "number",
-        position: "bottom",
-        gridStyle: [
-          {
-            stroke: "rgba(219, 219, 219, 1)",
-            lineDash: [4, 2],
-          },
-        ],
-      },
-      {
-        type: "number",
-        position: "left",
-        gridStyle: [
-          {
-            stroke: "rgba(219, 219, 219, 1)",
-            lineDash: [4, 2],
-          },
-        ],
-      },
-    ],
+    series: generateChartData(clusters)
   });
 
   return (
